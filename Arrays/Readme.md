5d9aa2bb3066ba658b684f9cfd820c0e4fe60327
Jean-Johnson <jeanjohnsonldrs@gmail.com>
2024-05-26 16:36:17 +0530

    array

---
+++ Arrays/Readme.md
@@ -0,0 +1,2 @@
+This is the basic data struct everyone should know about
+In python we use List which is faster than array in the array module
---
+++ Arrays/list_imp.py
@@ -0,0 +1,9 @@
+array = []
+print(f"The Value of the list => {array}")
+array.append(1)
+array.append(2)
+array.append(3)
+array.append(4)
+print(f"After insertion => {array}")
+array.remove(3)
+print(f"After Deletion => {array}")
